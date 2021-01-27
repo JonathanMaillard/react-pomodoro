b884f6a463335ba12663bbef68e7fefd8c01b9ab
JonathanMaillard <jonathan.maillard95@gmail.com>
2021-01-27 12:28:43 +0100

    test7

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//console.log('hello world');
+
+//console.log("hiil");
+
+/*import React from "react";
+import { render } from "react-dom";
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello {this.props.name}
+        </div>
+      );
+    }
+  }
+  
+  render(
+    <HelloMessage name="Taylor" />,
+    document.getElementById('hello-example')
+  );*/
+
+//console.log("yo");
